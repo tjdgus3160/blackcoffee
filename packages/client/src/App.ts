84866959e594102f010bf873c86a9e5375e72c68
tjdgus3160 <tjdgus3160@gmail.com>
2023-01-11 11:39:31 +0900

    add(client): Navigator 컴포넌트

--- packages/client/src/App.ts
+++ packages/client/src/App.ts
@@ -1,16 +1,24 @@
 import Component from '@core/Component';
 
+import Navigator from './components/Navigator';
+
 export default class App extends Component {
   template(): string {
     return `
     <div id="app" class="px-4">
       <div class="d-flex justify-center mt-5 w-100">
         <div class="w-100">
-          <header class="my-4"></header>
+          <header class="my-4" data-component="navigator"></header>
           <main class="mt-10 d-flex justify-center"></main>
         </div>
       </div>
     </div>
         `;
+  }
+
+  mounted(): void {
+    const $navigator = this.$target.querySelector('[data-component="navigator"]') as HTMLElement;
+
+    new Navigator($navigator);
   }
 }
---
+++ packages/client/src/components/Navigator.ts
@@ -0,0 +1,35 @@
+import {map} from 'lodash-es';
+
+import Component from '@core/Component';
+import {MenuType} from '@types';
+
+export default class Navigator extends Component {
+  state!: Record<MenuType, string>;
+
+  setup(): void {
+    this.state = {
+      espresso: '☕ 에스프레소',
+      frappuccino: '🥤 프라푸치노',
+      blended: '🍹 블렌디드',
+      teavana: '🫖 티바나',
+      desert: '🍰 디저트',
+    };
+  }
+
+  template(): string {
+    return `
+        <a href="/" class="text-black">
+            <h1 class="text-center font-bold">🌝 문벅스 메뉴 관리</h1>
+        </a>
+        <nav class="d-flex justify-center flex-wrap">
+            ${map(
+              this.state,
+              (value, key) =>
+                `<button data-category-name="${key}" class="cafe-category-name btn bg-white shadow mx-1">
+                    ${value}
+                  </button>`,
+            ).join('')}
+        </nav>
+        `;
+  }
+}
