6d37b763ee7c0621c1b7a38acbe561dfbdee16c0
tjdgus3160 <tjdgus3160@gmail.com>
2023-01-12 22:07:34 +0900

    feat(client): 메뉴추가

--- packages/client/src/components/MenuForm.tsx
+++ packages/client/src/components/MenuForm.tsx
@@ -1,8 +1,34 @@
+import {ChangeEvent, FormEvent, useState} from 'react';
+import {isEmpty} from 'lodash-es';
+
+import {useAppDispatch, useAppSelector} from '@hooks';
+import {addMenu, selectCurrentCategory} from '@store/menuSlice';
+
 const MenuForm = () => {
+  const [name, setName] = useState('');
+  const dispatch = useAppDispatch();
+  const currentCategory = useAppSelector(selectCurrentCategory);
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    if (isEmpty(name)) {
+      return;
+    }
+
+    dispatch(addMenu({currentCategory, name}));
+
+    setName('');
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="d-flex w-100">
-        <input className="input-field" placeholder="메뉴 이름" />
+        <input className="input-field" placeholder="메뉴 이름" value={name} onChange={onChange} />
         <button className="input-submit bg-green-600 ml-2">확인</button>
       </div>
     </form>
