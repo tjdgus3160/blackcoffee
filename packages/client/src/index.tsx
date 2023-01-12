b52ef705670685cfa42827079966bdac5a7c5773
tjdgus3160 <tjdgus3160@gmail.com>
2023-01-12 19:43:35 +0900

    feat(client): store 연결

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {store} from 'store';
 
 import App from './App';
 
@@ -10,6 +12,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
