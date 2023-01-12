edf96be074ace1be1b99d04a9d8297eca140a633
tjdgus3160 <tjdgus3160@gmail.com>
2023-01-12 20:02:54 +0900

    refactor(client): 불필요한 속성들 제거

--- packages/client/src/components/MenuForm.tsx
+++ packages/client/src/components/MenuForm.tsx
@@ -2,14 +2,8 @@ const MenuForm = () => {
   return (
     <form>
       <div className="d-flex w-100">
-        <label htmlFor="menu-name" className="input-label" hidden>
-          에스프레소 메뉴 이름
-        </label>
-        <input type="text" id="menu-name" name="espressoMenuName" className="input-field" placeholder="메뉴 이름" />
-        <button type="button" name="submit" id="menu-submit-button" className="input-submit bg-green-600 ml-2">
-          확인
-        </button>
-        <input type="hidden" />
+        <input className="input-field" placeholder="메뉴 이름" />
+        <button className="input-submit bg-green-600 ml-2">확인</button>
       </div>
     </form>
   );
--- packages/client/src/components/MenuHeader.tsx
+++ packages/client/src/components/MenuHeader.tsx
@@ -6,11 +6,9 @@ const MenuHeader = () => {
   const {currentCategory} = useAppSelector(selectMenu);
 
   return (
-    <div id="menu-header" className="heading d-flex justify-between">
-      <h2 id="category-title" className="mt-1">
-        {CATEGORY[currentCategory]} 메뉴 관리
-      </h2>
-      <span className="mr-2 mt-4 menu-count">총 0개</span>
+    <div className="d-flex justify-between">
+      <h2 className="mt-1">{CATEGORY[currentCategory]} 메뉴 관리</h2>
+      <span className="mr-2 mt-4">총 0개</span>
     </div>
   );
 };
