690b47a6c5fb7a2bca8baefeff2a61c5c41ba244
tjdgus3160 <tjdgus3160@gmail.com>
2023-01-12 20:27:12 +0900

    feat(client): app에서 메뉴데이터 불러오기 추가

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,20 @@
+import {useEffect} from 'react';
+
 import MenuForm from '@components/MenuForm';
 import MenuHeader from '@components/MenuHeader';
 import MenuList from '@components/MenuList';
 import Navigator from '@components/Navigator';
+import {useAppDispatch, useAppSelector} from '@hooks';
+import {fetchMenu, selectCurrentMenu} from '@store/menuSlice';
 
 function App() {
+  const currentMenu = useAppSelector(selectCurrentMenu);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMenu(currentMenu));
+  }, []);
+
   return (
     <div id="app" className="px-4">
       <div className="d-flex justify-center mt-5 w-100">
