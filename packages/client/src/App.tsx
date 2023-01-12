553ba426a4a63144fa0f667c474bf852f950c8c4
tjdgus3160 <tjdgus3160@gmail.com>
2023-01-12 19:23:40 +0900

    add(client): Navigator

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,18 @@
+import Navigator from '@components/Navigator';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div id="app" className="px-4">
+      <div className="d-flex justify-center mt-5 w-100">
+        <div className="w-100">
+          <Navigator />
+          <main className="mt-10 d-flex justify-center">
+            <div className="wrapper bg-white p-10"></div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ packages/client/src/components/Navigator.tsx
@@ -0,0 +1,22 @@
+import {map} from 'lodash-es';
+
+import {CATAGORY} from '@utils';
+
+const Navigator = () => {
+  return (
+    <header className="my-4">
+      <a href="/" className="text-black">
+        <h1 className="text-center font-bold">🌝 문벅스 메뉴 관리</h1>
+      </a>
+      <nav className="d-flex justify-center flex-wrap">
+        {map(CATAGORY, (value, key) => (
+          <button key={key} className="cafe-category-name btn bg-white shadow mx-1">
+            {value}
+          </button>
+        ))}
+      </nav>
+    </header>
+  );
+};
+
+export default Navigator;
