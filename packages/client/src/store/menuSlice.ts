8ce7e68e4356366d9fbfe849f6b083ab9192b31a
tjdgus3160 <tjdgus3160@gmail.com>
2023-01-12 19:57:37 +0900

    feat(client): menus 필드 생성

--- packages/client/src/store/menuSlice.ts
+++ packages/client/src/store/menuSlice.ts
@@ -1,13 +1,22 @@
 import {createSlice} from '@reduxjs/toolkit';
 import {RootState} from '@store/index';
+import {MenuItem} from '@types';
 import {MenuType} from '@utils';
 
 export interface State {
   currentCategory: MenuType;
+  menus: MenuItem[];
 }
 
 const initialState: State = {
   currentCategory: MenuType.espresso,
+  menus: [
+    {
+      id: '123',
+      name: 'asdf',
+      isSoldOut: true,
+    },
+  ],
 };
 
 export const menuSlice = createSlice({
@@ -18,3 +27,4 @@ export const menuSlice = createSlice({
 
 export const selectMenu = (state: RootState) => state.menu;
 export const selectCurrentCategory = (state: RootState) => state.menu.currentCategory;
+export const selectMenus = (state: RootState) => state.menu.menus;
