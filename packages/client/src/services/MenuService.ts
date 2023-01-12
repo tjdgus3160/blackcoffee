d05c3576e5108d38cd7f05c58a6320b158c8c516
tjdgus3160 <tjdgus3160@gmail.com>
2023-01-12 20:22:40 +0900

    add(client): menuservice

---
+++ packages/client/src/services/MenuService.ts
@@ -0,0 +1,46 @@
+import {MenuItem} from '@types';
+import {MenuType} from '@utils';
+
+const BASE_URL = 'http://localhost:5000/api';
+
+const option = (method: string, data?: any) => ({
+  method,
+  headers: {'Content-Type': 'application/json'},
+  body: data ? JSON.stringify(data) : null,
+});
+
+export default class MenuService {
+  static async getAllMenuByCategory(category: MenuType): Promise<MenuItem[]> {
+    return this.request(`/category/${category}/menu`);
+  }
+
+  static async createMenu(category: MenuType, name: string): Promise<MenuItem> {
+    return this.request(`/category/${category}/menu`, option('POST', {name}));
+  }
+
+  static async updateMenu(category: MenuType, menuId: string, name: string) {
+    return this.request(`/category/${category}/menu/${menuId}`, option('PUT', {name}));
+  }
+
+  static async toggleSoldOutMenu(category: MenuType, menuId: string) {
+    return this.request(`/category/${category}/menu/${menuId}/soldout`, option('PUT'));
+  }
+
+  static async deleteMenu(category: MenuType, menuId: string) {
+    return this.request(`/category/${category}/menu/${menuId}`, option('DELETE'));
+  }
+
+  static async request(url: string, option?: any) {
+    const response = await fetch(`${BASE_URL}${url}`, option);
+
+    if (!response.ok) {
+      const error = await response.json();
+
+      alert(error.message);
+
+      return;
+    }
+
+    return option?.method === 'DELETE' ? response : response.json();
+  }
+}
